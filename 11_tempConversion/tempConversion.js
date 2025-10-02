525112dd98637f1bb260f74b9d0a67ed52106f4e
Nisha Kumari <nishakumariujjwal@gmail.com>
2025-10-02 21:09:22 +0530

    Leap Year and Temp Conversion Done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fTemp) {
+  if (fTemp === -40) {
+    return fTemp;
+  } else return Math.round((fTemp - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cTemp) {
+  if (cTemp === -40) {
+    return cTemp;
+  } else return Math.round((cTemp * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
