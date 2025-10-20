2c42e80717a034ea85778976a9070d3d54c23206
Nisha Kumari <nishakumariujjwal@gmail.com>
2025-10-20 15:25:20 +0530

    13 Palindromes Finished

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str1) {
+  let alphanumerics = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let palindrome = str1
+    .toLowerCase()
+    .split("")
+    .filter((character) => alphanumerics.includes(character))
+    .join("");
+  let reverse = palindrome.split("").reverse().join("");
+  if (reverse == palindrome) {
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
