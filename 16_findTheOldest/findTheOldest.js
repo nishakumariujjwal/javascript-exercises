4ee6a9fe5df213eeb9216b2f3e0cb808bcaba00a
Nisha Kumari <nishakumariujjwal@gmail.com>
2025-10-22 10:28:13 +0530

    15 and 16 Done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let newArray = [];
+  let ages = people.map((person) => {
+    if (person.yearOfDeath == undefined) {
+      const currentYear = new Date().getFullYear();
+      newArray.push({
+        name: person.name,
+        age: currentYear - person.yearOfBirth,
+      });
+    } else {
+      newArray.push({
+        name: person.name,
+        age: person.yearOfDeath - person.yearOfBirth,
+      });
+    }
+  });
+  return newArray.sort((a, b) => b.age - a.age)[0];
 };
 
 // Do not edit below this line
